370c8a01a5f90d6bf5e8feb663520ae11f7f76ff
kmurimi-devs <kmurimi.devs@gmail.com>
2023-03-16 16:06:27 -0700

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *puts c
+ *main - function start
+ *return - zero as it is a print/puts function
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
