4cc03f5ec223f84e23aa5598028d6dd7fa8a191c
kmurimi-devs <kmurimi.devs@gmail.com>
2023-03-16 16:55:11 -0700

    standard output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  *main - initialise function
+  *Return: 1 as it prints to standard error (success)
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
