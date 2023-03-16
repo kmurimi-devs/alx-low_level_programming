bfbc9f1cacc4494e3847db1c4d0ec1e3ffd80faa
kmurimi-devs <kmurimi.devs@gmail.com>
2023-03-16 16:19:26 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - start function
+  *Return: zero as it prints to terminal
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
