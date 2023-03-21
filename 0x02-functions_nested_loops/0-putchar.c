41cf74b89aa51a43dd7a0ff6bd76b0f256341055
kmurimi-devs <kmurimi.devs@gmail.com>
2023-03-21 15:17:21 -0700

    create workspace and putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *main - calls putchar
+  *_putchar - prints letters
+  *Return: 0 for success
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
