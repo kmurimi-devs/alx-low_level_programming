995230ed6939089e27f6448a6249a9d616d47f43
kmurimi-devs <kmurimi.devs@gmail.com>
2023-03-21 16:30:58 -0700

    sign checker

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  *print_sign - prints sign of int input
+  *@n: number to be checked
+  *Return: 0 if positive, 1 if zero, -1 if negative
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+}
