ce5b4500f2e10230bf6ca6816a65e61fc055e2e8
kmurimi-devs <kmurimi.devs@gmail.com>
2023-03-29 10:52:56 -0700

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#includes "main.h"
+/**
+  * puts2 - print one character of two from the first one
+  * @str: input string
+  * Return: print
+  */
+void puts2(char *str)
+{
+	int len = 0;
+	int i = 0;
+	char *x = str;
+	int y;
+
+	while (*x != '\0')
+	{
+		x++;
+		len++;
+	}
+	i = len - 1;
+	for (y = 0; y <= i; y++)
+	{
+		if (y % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n');
+}
