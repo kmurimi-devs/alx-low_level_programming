020a8c57261813f004d78866fed043715450e4ff
kmurimi-devs <kmurimi.devs@gmail.com>
2023-03-29 02:48:03 -0700

    number swapper

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *swap_int - swaps values of input integers
+  *@a: int to swap
+  *@b: int to swap
+  */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
+
