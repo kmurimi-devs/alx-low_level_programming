f44a1ce35882000ce398041d2a319544eb67c1ec
kmurimi-devs <kmurimi.devs@gmail.com>
2023-04-04 04:06:57 -0700

    print numbers from zero to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - prints numbers from 0 - 9
+  * Return: zero
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf('\n');
+	return (0);
+}
