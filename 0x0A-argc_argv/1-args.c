ee6ba6b03888f65dcd6da8c7f2d0a63688e8ed97
kmurimi-devs <kmurimi.devs@gmail.com>
2023-04-17 10:38:44 -0700

    Argc, argv project files added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - print the number of args passed
+  *@argc: count of arguements passed to function
+  *@argv: arguements passed to a function as an array
+  *Return: 0 if successful
+  */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d", argc - 1);
+	return (0);
+}
