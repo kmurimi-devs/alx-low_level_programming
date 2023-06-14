d5b9d6ad701a7ac3bffbef62e5cf163b1346a4e2
kmurimi-devs <kmurimi-devs@gmail.com>
2023-06-14 06:26:43 -0700

    copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - copy a string and save it in a new location
+ * @str: string to be copied
+ * Return: pointer to copu or Null
+ */
+char *_strdup(char *str)
+{
+	char *copied;
+	int i = 0;
+	int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	copied = malloc(sizeof(char) * (i + 1));
+	if (copied == NULL)
+		return (NULL);
+	for(;str[j];j++)
+		copied[j] = str[j];
+	return (copied);
+}
