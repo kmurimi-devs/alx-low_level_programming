347ce4fcdd73279f5a3f01a1a32616e966d28551
kmurimi-devs <kmurimi-devs@gmail.com>
2023-06-21 01:46:39 -0700

    reorganise

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,10 +5,10 @@
 #define ERR_MSG "Error"
 
 /**
- * is_digit - checks if a string contains a non-digit char
+ * is_digit - checks if a string contains only digits
  * @s: string to be evaluated
  *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * Return: 1 if all characters are digits, 0 otherwise
  */
 int is_digit(char *s)
 {
@@ -17,10 +17,10 @@ int is_digit(char *s)
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
-			return (0);
+			return 0;
 		i++;
 	}
-	return (1);
+	return 1;
 }
 
 /**
@@ -31,19 +31,18 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int length = 0;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	while (s[length] != '\0')
+		length++;
+
+	return length;
 }
 
 /**
- * errors - handles errors for main
+ * handle_errors - handles errors in the program
  */
-void errors(void)
+void handle_errors(void)
 {
 	printf("Error\n");
 	exit(98);
@@ -51,51 +50,60 @@ void errors(void)
 
 /**
  * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
  *
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	char *num1, *num2;
+	int len1, len2, total_len, i, carry, digit1, digit2, *result, isLeadingZero = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
+	num1 = argv[1], num2 = argv[2];
+	if (argc != 3 || !is_digit(num1) || !is_digit(num2))
+		handle_errors();
+
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	total_len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * total_len);
 	if (!result)
-		return (1);
+		return 1;
+
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
+
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = num1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+
+		for (len2 = _strlen(num2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = s2[len2] - '0';
+			digit2 = num2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
+
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
+
+	for (i = 0; i < total_len - 1; i++)
 	{
 		if (result[i])
-			a = 1;
-		if (a)
+			isLeadingZero = 1;
+
+		if (isLeadingZero)
 			_putchar(result[i] + '0');
 	}
-	if (!a)
+
+	if (!isLeadingZero)
 		_putchar('0');
+
 	_putchar('\n');
 	free(result);
-	return (0);
+	return 0;
 }
